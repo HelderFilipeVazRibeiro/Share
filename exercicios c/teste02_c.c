55067874382591942c37a2220cbcc84532c9c0bf
Helder Ribeiro <helder.ribeiro@orangedynamic.pt>
2023-11-24 11:00:30 +0000

    _

---
+++ exercicios c/teste02_c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+/*
+printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+  return 0;
+
+ printf("Hello World!\n\n");
+  printf("I am learning C.");
+  return 0;
+*/
+
+
+
+
+}
+
+
+
