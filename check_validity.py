57dd142d261c7cb95196464ebf0125dbe513bc97
Hélder Ribeiro <helder.ribeiro91@gmail.com>
2023-11-27 18:42:02 +0000

    traduction

--- check_validity.py
+++ check_validity.py
@@ -1,14 +1,14 @@
-chave = '45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd'
-chave_2 = '45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd'
-if chave == chave_2:
-    print("As chaves são iguais.")
+key = '45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd'
+key_2 = '45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd'
+if key == key_2:
+    print("keys it's Ok. Well Done!")
 else:
-    print("As chaves são diferentes.")
-
-#download
-#sacar e guardar chave sha-512
-#guardar código gerado: 45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd  Apache-NetBeans-18-bin-macosx.dmg
-#quando abrir o terminal arrastar a pasta para assumir o directório
-#comando para terminal (mac): shasum -a 512 Apache-NetBeans-18-bin-macosx.dmg
-#resultado terminal: 45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd
-#script python comparação
+    print("Destroy this shit.")
+#_______________________________ Instructions ___________________________________
+#download: https://netbeans.apache.org/front/main/download/nb18
+#save key: SHA-512
+#save generated key: 45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd  Apache-NetBeans-18-bin-macosx.dmg
+#When open the terminal, drag the folder to take over the directory
+#command for Terminal Sheel MacOs: shasum -a 512 Apache-NetBeans-18-bin-macosx.dmg
+#terminal result: 45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd
+#script python comparation
