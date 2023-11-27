d62bebda7bcdefe383f70385cb6f4c31c90cc48f
Helder Ribeiro <helder.ribeiro@orangedynamic.pt>
2023-11-27 17:51:20 +0000

    _

---
+++ Helloworld.py
@@ -0,0 +1,16 @@
+red_bucket = "kevin"
+red_bucket = 10
+# del red_bucket --> delete variable
+
+Thomas_Age = 3
+Age_at_Kindergarten = 5
+
+if Thomas_Age < Age_at_Kindergarten:
+    print("Thomas should be in pre-school")
+elif Thomas_Age == Age_at_Kindergarten:
+    print("Enjoy Kindergarten")
+else:
+    print("Thomas should be in kindergartern or another class")
+        
+        
+        
---
+++ check_validity.py
@@ -0,0 +1,13 @@
+chave = '45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd'
+chave_2 = '45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd'
+if chave == chave_2:
+    print("As chaves são iguais.")
+else:
+    print("As chaves são diferentes.")
+
+#download
+#sacar e guardar chave sha-512
+#guardar código gerado: 45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd  Apache-NetBeans-18-bin-macosx.dmg
+#comando para terminal (mac): shasum -a 512 Apache-NetBeans-18-bin-macosx.dmg
+#resultado terminal: 45ae189936973612cdaefca127f7664257081a3794c84c28213d395f5e3cf46910bafb6f1d89a0e2466c5dc35481a9acde1be4506cf55ff356a0404052529cdd
+#script python comparação
