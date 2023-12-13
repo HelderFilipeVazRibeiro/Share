bafa3c8d9d6254e10ca2a12323d50952fd62eef7
Helder Ribeiro <helder.ribeiro@orangedynamic.pt>
2023-12-13 19:58:40 +0000

    _

--- mario.py
+++ mario.py
@@ -1,4 +1,4 @@
-"""
+
 def main():
     print_column(4)
 
@@ -7,7 +7,6 @@ def print_column(height):
     
 main()
 """
-
 def main():
     print_row(4)
 
@@ -15,3 +14,20 @@ def print_row(width):
     print("#" * width, end=" \n")
     
 main()
+"""
+"""
+def main():
+    print_square(5)
+
+def print_square(size):
+    #for each row in square
+    for i in range(size):
+        #for each brick in row
+        for j in range(size):
+            #print brick
+            print("#", end="")
+        print()
+
+main()
+
+"""
