6d0114aa9bcbda2b228224c102afd931fb99aa36
HelderFilipeVazRibeiro <helder.ribeiro@orangedynamic.pt>
2023-12-14 15:19:48 +0000

    update

--- itunes.py
+++ itunes.py
@@ -5,6 +5,9 @@ import sys
 if len(sys.argv) != 2:
         sys.exit()
 # url original: https://itunes.apple.com/search?entity=song&limit=1&term=weezer
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
 
-print(json.dumps(response.json(), indent=2))
+#print(json.dumps(response.json(), indent=2))
+o = response.json()
+for result in o["results"]:
+        print(result["trackName"])
